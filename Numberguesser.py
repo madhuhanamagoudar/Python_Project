d781f6691db8435c60825496119650a4cc971d10
madhuhanamagoudar <56438197+madhuhanamagoudar@users.noreply.github.com>
2020-08-23 15:29:20 +0530

    Add files via upload

--- Numberguesser.py
+++ Numberguesser.py
@@ -1,4 +1,3 @@
-print("I am thinking of a number ")
 import random
 def guesss_num(): #function for guessing the number
     print("Try to guess that number")
@@ -12,10 +11,16 @@ def guesss_num(): #function for guessing the number
             print("Number is too low")
         else:
             break
-secretnumber=random.randint(1,50) #Creates a random integer
-guesss_num() #calling the guess number program
-if(guess== secretnumber):
-    print("Congratulations!! You guessed it right ")
+choice=input("You want to play?")
+while(choice == "yes"):
+    print("I am Currently Thinking of a number")
+    secretnumber=random.randint(1,50) #Creates a random integer
+    guesss_num()
+    if(guess== secretnumber):
+        print("Congratulations!! You guessed it right ")
+    else:
+        print("Better Luck next time ")
+        print("The actual number was "+str(secretnumber)) #says what was the number
+    choice=input("You Want to Continue?")
 else:
-    print("Better Luck next time ")
-    print("The actual number was "+str(secretnumber)) #says what was the number
+    exit()
